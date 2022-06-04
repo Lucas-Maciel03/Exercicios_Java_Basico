8f590fbe8ae3034549cb245764aee44d309d8e1e
Lucas Maciel <farias.maciel@outlook.com>
2022-06-04 14:37:14 -0400

    atualizando arquivo sobre espressão ternaria

--- src/estrutura_condicional/expressaoTernaria.java
+++ src/estrutura_condicional/expressaoTernaria.java
@@ -3,7 +3,12 @@ package estrutura_condicional;
 public class expressaoTernaria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double preco = 34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		//(condição) ? valor_se_verdadeiro : valor_se_falso
+		
+		System.out.println(desconto);
 
 	}
 
